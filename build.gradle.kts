281ab55cf1006cde82eb1a59e60e78c42969cad4
sayaya <sayaya1090@hotmail.com>
2024-04-18 07:56:29 +0900

    infra: github actions 추가
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가
    
    infra: github actions 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.23" apply false
     id("org.springframework.boot") version "3.2.4" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
-    id("com.google.cloud.tools.jib") version "3.4.0" apply false
+    id("com.google.cloud.tools.jib") version "3.4.2" apply false
     id("net.sayaya.gwt") version "1.1.19" apply false
 }
 java.sourceCompatibility = JavaVersion.VERSION_21
