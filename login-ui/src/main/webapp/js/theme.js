c9204d3851ed3cf749db2da2d6b8320bceb1001c
sayaya <sayaya1090@hotmail.com>
2024-05-01 07:19:55 +0900

    script: 정적 파일 삭제

--- login-ui/src/main/webapp/js/theme.js
+++
@@ -1,6 +0,0 @@
-let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-document.documentElement.setAttribute('color-theme', theme);
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
-    theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    document.documentElement.setAttribute('color-theme', theme);
-})
