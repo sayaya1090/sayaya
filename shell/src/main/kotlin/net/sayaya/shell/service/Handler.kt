3b7a66ac7427f8b366a3280d0bcd3652134e7f27
sayaya <sayaya1090@hotmail.com>
2024-06-08 16:03:18 +0900

    test: 메뉴 출력 지연 현상 확인을 위한 로그 추가

--- shell/src/main/kotlin/net/sayaya/shell/service/Handler.kt
+++ shell/src/main/kotlin/net/sayaya/shell/service/Handler.kt
@@ -1,13 +1,18 @@
 package net.sayaya.shell.service
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient
 import org.springframework.http.HttpCookie
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.toEntity
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
+import java.time.LocalDateTime
 
 @Service("net.sayaya.service.Handler")
 class Handler(
@@ -17,16 +22,23 @@ class Handler(
     fun list(cookies: Map<String, List<HttpCookie>>): Flux<Any> {
         val cookiesMap = cookies.map { e->e.key to e.value.stream().map { it.value }.toList() }.toMap()
         return discovery.services
-            .flatMap { svc -> routes(svc, cookiesMap) }
+            .flatMap { svc ->
+                println("${LocalDateTime.now()} Service: $svc")
+                routes(svc, cookiesMap) }
             .onErrorContinue(Exception::class.java) { e, _ -> e.printStackTrace() }
     }
     fun routes(service: String, cookies: Map<String, List<String>>): Mono<Any> = discovery
         .getInstances(service).next()
-        .flatMap { svc -> client.get().uri(svc.uri.resolve("/menu"))
+        .flatMap { svc ->
+            println("${LocalDateTime.now()} Service picked: $svc")
+            client.get().uri(svc.uri.resolve("/menu"))
             .cookies{ cookies.forEach { cookie -> it.addAll(cookie.key, cookie.value) } }
             .retrieve()
             .toEntity<Any>()
-            .filter { it.statusCode.is2xxSuccessful }
+            .onErrorResume(WebClientResponseException::class.java) { if(it.statusCode == HttpStatus.UNAUTHORIZED) Mono.empty() else Mono.error(it) }
+            .filter {
+                println("${LocalDateTime.now()} Service: $svc, statusCode: ${it.statusCode}")
+                it.statusCode.is2xxSuccessful }
             .map { it.body!! }
             .timeout(Duration.ofMillis(1500))
         }
